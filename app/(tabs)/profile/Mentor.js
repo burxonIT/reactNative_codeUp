3547d6c9b64a38d11ee0c708db45bdf0f75d0b26
burxonIT <116946122+burxonIT@users.noreply.github.com>
2023-07-07 17:51:10 +0300

    wdde
    efsfe

--- app/(tabs)/profile/Mentor.js
+++ app/(tabs)/profile/Mentor.js
@@ -1,12 +1,264 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import React from 'react'
 
-const Mentor = () => {
+const index = () => {
   return (
-    <View>
-      <Text>Mentor</Text>
-    </View>
+    <ScrollView>
+      <View>
+
+        <View style={{ flexDirection: 'row', alignItems: 'center' , marginTop:10, marginLeft:10}}>
+          <View style={{
+            width: 80,
+            height: 80,
+            backgroundColor: 'white',
+            shadowColor: "#000",
+            shadowOffset: { width: 3, height: 3 },
+            shadowOpacity: 0.3,
+            shadowRadius: 3.84,
+            borderRadius: 50,
+            justifyContent: 'center',
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}>
+            <View style={{ width: 70, height: 70, backgroundColor: '#EF662A33', borderRadius: 50, justifyContent: 'center', flexDirection: 'row', alignItems: 'center' }}>
+              <Image
+                source={require("../../img/html5-without-wordmark-color.png")}
+              />
+            </View>
+          </View>
+          <View
+          style={{
+            marginLeft:15
+          }}>
+
+          <Text
+           style={{
+            color: 'black',
+            fontSize: 24
+          }}
+          >HTML</Text>
+          <Text style={{
+            color: '#9DA7BB',
+            fontSize: 12,
+            width:130
+          }}>Lorem ipsum dolor sit amet, consectetur ar.</Text>
+          </View>
+        </View>
+
+
+        
+
+        <View style={{ flexDirection: 'row', alignItems: 'center' , marginTop:10, marginLeft:10}}>
+          <View style={{
+            width: 80,
+            height: 80,
+            backgroundColor: 'white',
+            shadowColor: "#000",
+            shadowOffset: { width: 3, height: 3 },
+            shadowOpacity: 0.3,
+            shadowRadius: 3.84,
+            borderRadius: 50,
+            justifyContent: 'center',
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}>
+            <View style={{ width: 70, height: 70, backgroundColor: '#CD679933', borderRadius: 50, justifyContent: 'center', flexDirection: 'row', alignItems: 'center' }}>
+              <Image
+                source={require("../../img/sass-seeklogo.com.png")}
+              />
+            </View>
+          </View>
+          <View
+          style={{
+            marginLeft:15
+          }}>
+
+          <Text
+           style={{
+            color: 'black',
+            fontSize: 24
+          }}
+          >Sass</Text>
+          <Text style={{
+            color: '#9DA7BB',
+            fontSize: 12,
+            width:130
+          }}>Lorem ipsum dolor sit amet, consectetur ar.</Text>
+          </View>
+        </View>
+
+        <View style={{ flexDirection: 'row', alignItems: 'center' , marginTop:10, marginLeft:10}}>
+          <View style={{
+            width: 80,
+            height: 80,
+            backgroundColor: 'white',
+            shadowColor: "#000",
+            shadowOffset: { width: 3, height: 3 },
+            shadowOpacity: 0.3,
+            shadowRadius: 3.84,
+            borderRadius: 50,
+            justifyContent: 'center',
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}>
+            <View style={{ width: 70, height: 70, backgroundColor: '#563D7C33', borderRadius: 50, justifyContent: 'center', flexDirection: 'row', alignItems: 'center' }}>
+              <Image
+                source={require("../../img/bootstrap-seeklogo.com.png")}
+              />
+            </View>
+          </View>
+          <View
+          style={{
+            marginLeft:15
+          }}>
+
+          <Text
+           style={{
+            color: 'black',
+            fontSize: 24
+          }}
+          >Bootstrap</Text>
+          <Text style={{
+            color: '#9DA7BB',
+            fontSize: 12,
+            width:130
+          }}>Lorem ipsum dolor sit amet, consectetur ar.</Text>
+          </View>
+        </View>
+
+
+
+
+
+        <View style={{ flexDirection: 'row', alignItems: 'center' , marginTop:10, marginLeft:10}}>
+          <View style={{
+            width: 80,
+            height: 80,
+            backgroundColor: 'white',
+            shadowColor: "#000",
+            shadowOffset: { width: 3, height: 3 },
+            shadowOpacity: 0.3,
+            shadowRadius: 3.84,
+            borderRadius: 50,
+            justifyContent: 'center',
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}>
+            <View style={{ width: 70, height: 70, backgroundColor: '#DE4C3633', borderRadius: 50, justifyContent: 'center', flexDirection: 'row', alignItems: 'center' }}>
+              <Image
+                source={require("../../img/git-seeklogo.com.png")}
+              />
+            </View>
+          </View>
+          <View
+          style={{
+            marginLeft:15
+          }}>
+
+          <Text
+           style={{
+            color: 'black',
+            fontSize: 24
+          }}
+          >GIT</Text>
+          <Text style={{
+            color: '#9DA7BB',
+            fontSize: 12,
+            width:130
+          }}>Lorem ipsum dolor sit amet, consectetur ar.</Text>
+          </View>
+        </View>
+
+
+
+        <View style={{ flexDirection: 'row', alignItems: 'center' , marginTop:10, marginLeft:10}}>
+          <View style={{
+            width: 80,
+            height: 80,
+            backgroundColor: 'white',
+            shadowColor: "#000",
+            shadowOffset: { width: 3, height: 3 },
+            shadowOpacity: 0.3,
+            shadowRadius: 3.84,
+            borderRadius: 50,
+            justifyContent: 'center',
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}>
+            <View style={{ width: 70, height: 70, backgroundColor: '#EA1B2233', borderRadius: 50, justifyContent: 'center', flexDirection: 'row', alignItems: 'center' }}>
+              <Image
+                source={require("../../img/spring-seeklogo.com.png")}
+              />
+            </View>
+          </View>
+          <View
+          style={{
+            marginLeft:15
+          }}>
+
+          <Text
+           style={{
+            color: 'black',
+            fontSize: 24
+          }}
+          >Spring</Text>
+          <Text style={{
+            color: '#9DA7BB',
+            fontSize: 12,
+            width:130
+          }}>Lorem ipsum dolor sit amet, consectetur ar.</Text>
+          </View>
+        </View>
+
+
+
+
+
+
+        <View style={{ flexDirection: 'row', alignItems: 'center' , marginTop:10, marginLeft:10}}>
+          <View style={{
+            width: 80,
+            height: 80,
+            backgroundColor: 'white',
+            shadowColor: "#000",
+            shadowOffset: { width: 3, height: 3 },
+            shadowOpacity: 0.3,
+            shadowRadius: 3.84,
+            borderRadius: 50,
+            justifyContent: 'center',
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}>
+            <View style={{ width: 70, height: 70, backgroundColor: '#DE4C3633', borderRadius: 50, justifyContent: 'center', flexDirection: 'row', alignItems: 'center' }}>
+              <Image
+                source={require("../../img/oracle-seeklogo.com.png")}
+              />
+            </View>
+          </View>
+          <View
+          style={{
+            marginLeft:15
+          }}>
+
+          <Text
+           style={{
+            color: 'black',
+            fontSize: 24
+          }}
+          >Oracle</Text>
+          <Text style={{
+            color: '#9DA7BB',
+            fontSize: 12,
+            width:130
+          }}>Lorem ipsum dolor sit amet, consectetur ar.</Text>
+          </View>
+        </View>
+
+
+
+      </View>
+    </ScrollView>
   )
 }
 
-export default Mentor
+export default index
